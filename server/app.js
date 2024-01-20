5f6f13531cc53b8479da0339ad4fb2684712a93e
Kage Gamis <kjgamis@gmail.com>
2024-01-20 09:36:25 -0500

    feat: add express

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('express-async-errors')
+const Problem = require('api-problem')
+const problemMiddleware = require('api-problem/lib/middleware')
+const errorHandlerMiddleware = require('./middleware/errorHandler')
+const interpreter = require('./routes/interpreter')
+const user = require('./routes/user')
+
+app.use(cors())
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
+app.use('/api/interpreters', interpreter)
+app.use('/api/user', user)
+app.use('*', () => {
+  throw new Problem(404, 'Route does not exist')
+})
+
+app.use(errorHandlerMiddleware)
+app.use(problemMiddleware)
+
+module.exports = app
